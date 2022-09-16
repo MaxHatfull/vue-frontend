5f00df8c18cfc846a6e0ad46679be908e31395c3
Katie Nelson <katie.nelson@freeagent.com>
2022-09-16 11:42:58 +0100

    Add styling and title to header

--- src/App.js
+++ src/App.js
@@ -40,6 +40,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Login loginSuccess={loginSuccess}/>
+        <div className="title">
+          <h1>Energy Usage</h1>
+          <h2>At Max's house</h2>
+        </div>
+        <form className="login" onSubmit={handleSubmit}>
+          <label>
+            <input placeholder="username" type="text" name="username" onChange={ e => setUsername(e.target.value) }/>
+          </label>
+          <label>
+            <input placeholder="password" type="password" name="password" onChange={ e => setPassword(e.target.value) }/>
+          </label>
+          <button type="submit">Login</button>
+        </form>
       </header>
       <div id="content">
         <Chart chartData={data} channelNames={channelNames}/>
