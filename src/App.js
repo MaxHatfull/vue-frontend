3ea74c7f2cfb8511d990275d98f418170ed78769
Max Hatfull <max.hatfull@freeagent.com>
2022-09-16 11:59:12 +0100

    get ALLLLLL the data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { Chart } from './Chart';
 
 function App() {
   const [token, setToken] = useState("");
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
+  const [channelNames, setChannelNames] = useState([])
 
   useEffect(() => {
     if(localStorage.getItem('tkn') !== ""){
@@ -18,24 +19,23 @@ function App() {
     console.log("data has updated")
   }, [data]);
 
-  const makeRequest = event => {
-    const request = new Request(token);
-    request.getDevices()
-    .then(devices => devices.devices[0].deviceGid)
-    .then(deviceGid => request.getChartUsage(deviceGid))
-    .then(data => setData(data));
-  }
-
   const loginSuccess = (token) => {
     setToken(token)
     console.log(token)
     const request = new Request(token);
     request.getDevices()
-    .then(devices => devices.devices[0].deviceGid)
-    .then(deviceGid => request.getChartUsage(deviceGid))
+    .then(devices => {
+      console.log(devices)
+      setChannelNames(devices.devices[0].devices[0].channels.map(c => c.name))
+      return {
+      gid: devices.devices[0].deviceGid,
+      channels: devices.devices[0].devices[0].channels.map(c => c.channelNum)
+    }})
+    .then(deviceData => deviceData.channels.map(c => request.getChartUsage(deviceData.gid, c)))
+    .then(requests => Promise.all(requests))
     .then(data => setData(data));
   }
-  
+
   return (
     <div className="App">
       <header className="App-header">
